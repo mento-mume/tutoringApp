3b588e2e1054e6b4c4aeb0a104cb9e2600a2b9a1
Mento Mume <akpankevman@gmail.com>
2021-05-13 07:18:13 +0100

    took out get with id

--- routes/tutors.js
+++ routes/tutors.js
@@ -36,12 +36,6 @@ router.get('/',async (req,res)=>{
      res.send(tutor);
  });
  
- router.get('/id:',async (req,res)=>{
-     const tutor = await Tutor.findById(req.params.id);
- 
-     if(!tutor) return res.status('404').send('the tutor with the id could not be found');
- 
-     res.send(tutor);
- });
+
  
  module.exports =router;
